66914134bf8486bdbbabcbf96edecd93f5627601
ToniK4 <toni.kneze4@gmail.com>
2021-09-29 18:40:11 +0200

    Added Superclass, changed the fields to actual properties. Left public so tests can access methods and properties.

---
+++ ClassLibraryTicketSystem/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassLibraryTicketSystem
+{
+    /// <summary>
+    /// Abstract superclass of vehicles.
+    /// </summary>
+    public abstract class Vehicle
+    {
+        /// <summary>
+        /// Returns a string of the vehicle's License plate.
+        /// </summary>
+        
+        public string Licenseplate { get; set; }
+        /// <summary>
+        /// Returns a DateTime of the vehicle's date of manufacture.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Abstract method for the price of the vehicle.
+        /// </summary>
+        /// <returns></returns>
+        public abstract double Price();
+
+        /// <summary>
+        /// Abstract method for the type of the vehicle.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string VehicleType();
+    }
+}
