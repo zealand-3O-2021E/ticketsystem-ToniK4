98718d7c21ff8b54201b6c5957005616da905ecd
ToniK4 <toni.kneze4@gmail.com>
2021-09-29 10:25:51 +0200

    Task 2 - Car class, tests

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassLibraryTicketSystem
+{
+    public class Car
+    {
+        public string Licenseplate;
+        public DateTime Date;
+
+        /// <summary>
+        /// Returns a double of the price of the car, which is set to 240.
+        /// </summary>
+        /// <returns></returns>
+        public double Price()
+        {
+            return 240;
+        }
+
+        /// <summary>
+        /// Returns a string of the type of the vehicle, which is "Car".
+        /// </summary>
+        /// <returns></returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ ClassLibraryTicketSystemTests/CarTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest_Is240()
+        {
+            //Arrange
+            Car car = new Car();
+
+            //Act
+            double actualValue = car.Price();
+
+            //Assert
+            Assert.AreEqual(240, actualValue);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest_IsCar()
+        {
+            //Arrange
+            Car car = new Car();
+
+            //Act
+            string actualValue = car.VehicleType();
+
+            //Assert
+            Assert.AreEqual("Car", actualValue);
+        }
+    }
+}
