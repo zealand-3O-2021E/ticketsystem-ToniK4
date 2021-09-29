92ccdfc4da27df807b18304837ce74a2a1ea7959
ToniK4 <toni.kneze4@gmail.com>
2021-09-29 10:32:17 +0200

    Task 3 - MC class and tests

---
+++ ClassLibraryTicketSystem/MC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    public class MC
+    {
+        public string Licenseplate;
+        public DateTime Date;
+
+        /// <summary>
+        /// Returns a double of the price of the MC, which is set to 125.
+        /// </summary>
+        /// <returns></returns>
+        public double Price()
+        {
+            return 125;
+        }
+
+        /// <summary>
+        /// Returns a string of the type of the vehicle, which is "MC".
+        /// </summary>
+        /// <returns></returns>
+        public string Vehicle()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ ClassLibraryTicketSystemTests/MCTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PriceTest_Is125()
+        {
+            //Arrange
+            MC MC = new MC();
+
+            //Act
+            double actualValue = MC.Price();
+
+            //Assert
+            Assert.AreEqual(125, actualValue);
+        }
+
+        [TestMethod()]
+        public void VehicleTest_IsMC()
+        {
+            //Arrange
+            MC MC = new MC();
+
+            //Act
+            string actualValue = MC.Vehicle();
+
+            //Assert
+            Assert.AreEqual("MC", actualValue);
+        }
+    }
+}
