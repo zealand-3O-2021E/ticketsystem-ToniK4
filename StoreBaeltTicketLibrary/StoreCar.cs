dd241e28b2b7cf26e1f2516e887dca32c923a701
ToniK4 <toni.kneze4@gmail.com>
2021-09-29 22:54:53 +0200

    Task 9 - It all works now, randomly. Spent 2 hours on this simple step.

---
+++ StoreBaeltTicketLibrary/StoreCar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ClassLibraryTicketSystem;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StoreCar : Car
+    {
+        private const double WeekendDiscount = 0.2;
+
+        /// <summary>
+        /// Price method after weekend and brobizz if applicable.
+        /// </summary>
+        /// <returns></returns>
+        public override double Price()
+        {
+            double FinalPrice = 240;
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                FinalPrice = FinalPrice * (1 - WeekendDiscount);
+            }
+
+            if (Brobizz)
+            {
+                FinalPrice = FinalPrice * (1 - _brobizzDiscount);
+            }
+
+            return FinalPrice;
+        }
+    }
+}
---
+++ StoreBaeltTicketLibraryTests/StoreCarTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreBaeltTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary.Tests
+{
+    [TestClass()]
+    public class StoreCarTests
+    {
+        [TestMethod()]
+        public void PriceTest_WithDiscountsIs182_4()
+        {
+            //Arrange
+            StoreCar car = new StoreCar() { Brobizz = true, Date = new DateTime(2021, 10, 02) };
+
+            //Act
+            double actualValue = car.Price();
+
+            //Assert
+            Assert.AreEqual(182.4, actualValue, 0.1);
+        }
+    }
+}
